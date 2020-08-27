3d230fda5c3f79160b33ff9a94f63be58ed65b5b
idjaeha <jaehanim1@gmail.com>
2020-08-28 02:57:19 +0900

    Add pingpong.c First Version

---
+++ greeting.c
@@ -0,0 +1,13 @@
+#include "types.h"
+#include "user.h"
+
+void greeting()
+{
+    printf(1, "Hello, world!!!\n");
+}
+
+int main(int argc, char *argv[])
+{
+    greeting();
+    exit();
+}
---
+++ pingpong.c
@@ -0,0 +1,77 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+// pingpong에 관한 궁금증
+// 1. 좀비 뜨는 문제 어떻게 해결?
+// 2. 핑퐁을 실행하면 할수록 계속 늘어나는 pid가 늘어나는 이유?
+
+// xv6에 관한 궁금증
+// 버퍼에 화살표와 같은 기호 들어가지 못하게 하는 법
+// printf는 한글자씩 출력되는 듯?
+
+// 알아둬야할 상식
+// pipe[0] : 읽기용
+// pipe[1] : 쓰기용
+
+char buf[512];
+
+void pingpong()
+{
+    int pipe_from_parent[2];
+    int pipe_from_child[2];
+
+    pipe(pipe_from_parent);
+    pipe(pipe_from_child);
+    if (fork() == 0) // child
+    {
+        char message[] = "0: received pong\n";
+        char char_pid = getpid() + '0';
+
+        // 읽기 부분
+        close(pipe_from_parent[1]);
+        read(pipe_from_parent[0], buf, sizeof(buf));
+        printf(1, buf);
+        close(pipe_from_parent[0]);
+
+        // 쓰기 부분
+        close(pipe_from_child[0]);
+        // 해당 방법은 앞의 자리 수가 일의 자리인 경우만 해당
+        memset(message, char_pid, sizeof(char));
+        write(pipe_from_child[1], message, sizeof(message));
+        close(pipe_from_child[1]);
+    }
+    else // parent
+    {
+        char message[] = "0: received ping\n";
+        char char_pid = getpid() + '0';
+
+        // 쓰기 부분
+        close(pipe_from_parent[0]);
+        // 해당 방법은 앞의 자리 수가 일의 자리인 경우만 해당
+        memset(message, char_pid, sizeof(char));
+        write(pipe_from_parent[1], message, sizeof(message));
+        close(pipe_from_parent[1]);
+
+        // 읽기 부분
+        close(pipe_from_child[1]);
+        read(pipe_from_child[0], buf, sizeof(buf));
+        printf(1, buf);
+        close(pipe_from_child[0]);
+
+        wait(); // 1. wait으로 해결하는 것이 현명?
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc <= 1)
+    {
+        pingpong();
+    }
+    else
+    {
+        printf(1, "Can't use parameters\n");
+    }
+    exit();
+}
